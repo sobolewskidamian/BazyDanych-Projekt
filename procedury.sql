63b4390e0fe14261041026c379371b5b83d9a094
Damian Sobolewski <damiansobolewski12@gmail.com>
2018-12-18 15:55:38 +0100

    Procedury

--- procedury.sql
+++ procedury.sql
@@ -56,3 +56,111 @@ BEGIN
 END
 
 
+CREATE PROCEDURE PROCEDURE_UpdateConferenceDetails(
+ @ConferenceID INT,
+ @Name VARCHAR(100),
+ @Place VARCHAR(100),
+ @DiscountForStudents FLOAT,
+ @Description VARCHAR(200))
+ AS
+BEGIN
+ IF EXISTS(
+  SELECT * FROM Conferences
+  WHERE @ConferenceID = ConferenceID
+ )
+ BEGIN
+  IF @Name IS NOT NULL
+  BEGIN
+   UPDATE Conferences
+   SET Name = @Name
+   WHERE ConferenceID = @ConferenceID;
+  END
+  IF @DiscountForStudents IS NOT NULL
+  BEGIN
+   UPDATE Conferences
+   SET DiscountForStudents = @DiscountForStudents
+   WHERE ConferenceID = @ConferenceID;
+  END
+  IF @Place IS NOT NULL
+  BEGIN
+   UPDATE Conferences
+   SET Place = @Place
+   WHERE ConferenceID = @ConferenceID;
+  END
+  IF @Description IS NOT NULL
+  BEGIN
+   UPDATE Conferences
+   SET Description = @Description
+   WHERE ConferenceID = @ConferenceID;
+  END
+ END
+END
+
+
+CREATE PROCEDURE PROCEDURE_UpdateWorkshopDetails(
+ @WorkshopID INT,
+ @Name VARCHAR(100),
+ @StartTime DATE,
+ @EndTime DATE,
+ @Cost DECIMAL(9,2),
+ @NumberOfParticipants INT)
+ AS
+BEGIN
+ IF EXISTS(
+  SELECT * FROM Workshops
+  WHERE @WorkshopID = WorkshopID
+ )
+ BEGIN
+  IF @Name IS NOT NULL
+  BEGIN
+   UPDATE Workshops
+   SET Name = @Name
+   WHERE WorkshopID = @WorkshopID;
+  END
+  IF @StartTime IS NOT NULL
+  BEGIN
+   UPDATE Workshops
+   SET StartTime = @StartTime
+   WHERE WorkshopID = @WorkshopID;
+  END
+  IF @EndTime IS NOT NULL
+  BEGIN
+   UPDATE Workshops
+   SET EndTime = @EndTime
+   WHERE WorkshopID = @WorkshopID;
+  END
+  IF @Cost IS NOT NULL
+  BEGIN
+   UPDATE Workshops
+   SET Cost = @Cost
+   WHERE WorkshopID = @WorkshopID;
+  END
+  IF @NumberOfParticipants IS NOT NULL
+  BEGIN
+   UPDATE Workshops
+   SET NumberOfParticipants = @NumberOfParticipants
+   WHERE WorkshopID = @WorkshopID;
+  END
+ END
+END
+
+
+CREATE PROCEDURE PROCEDURE_AddConferenceBooking(
+ @ConferenceID INT,
+ @ClientID INT,
+ @BookingDate DATE)
+ AS
+BEGIN
+ IF @BookingDate IS NULL
+  BEGIN
+   INSERT INTO ConferenceBooking (Conferences_ConferenceID, Clients_ClientID)
+   VALUES (@ConferenceID, @ClientID);
+  END
+ ELSE
+  BEGIN
+   INSERT INTO ConferenceBooking (Conferences_ConferenceID, BookingDate, Clients_ClientID)
+   VALUES (@ConferenceID, @BookingDate, @ClientID);
+  END
+END
+
+
